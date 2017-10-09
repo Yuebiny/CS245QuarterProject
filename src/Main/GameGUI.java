7149cc8d3612c9e96d09ab46ea31bf2f8dcfc8f5
coboba123 <cnguyen235@yahoo.com>
2017-10-08 22:44:00 -0700

    HelpFrame

--- src/Main/GameGUI.java
+++ src/Main/GameGUI.java
@@ -130,7 +130,6 @@ public class GameGUI extends javax.swing.JFrame {
     //Method: helpButton
     //purpose: this method diposes the current frame and opens the Help menu, used for game instructions.
     private void helpButton(){
-        dispose();
         new HelpFrame().setVisible(true);
     }
     
@@ -191,20 +190,12 @@ public class GameGUI extends javax.swing.JFrame {
         datePlaceHolder.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         datePlaceHolder.setText("DATEPLACEHOLDER");
         gamePanel.add(datePlaceHolder);
-
-        datePlaceHolder.setBounds(400, 0, 100, 26);
-
         datePlaceHolder.setBounds(400, 0, 100, 14);
-
 
         timePlaceHolder.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         timePlaceHolder.setText("TIMEPLACEHOLDER");
         gamePanel.add(timePlaceHolder);
-
-        timePlaceHolder.setBounds(510, 0, 70, 26);
-
         timePlaceHolder.setBounds(510, 0, 70, 14);
-
 
         hangPanel.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         gamePanel.add(hangPanel);
--- src/Main/HelpFrame.java
+++ src/Main/HelpFrame.java
@@ -20,34 +20,51 @@ public class HelpFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
+        jButton2 = new javax.swing.JButton();
+
+        jButton1.setText("Exit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAlwaysOnTop(true);
 
+        jTextPane1.setEditable(false);
+        jTextPane1.setText("How To Play:\n\nHangman is a guessing game where you need to guess letters until you have found the correct word. Once you select a letter, that letter will either be filled into the answer or a piece of the \"Hangman\" will be drawn. You win if you guess all the letters correctly before the \"Hangman\" is completely drawn, and you lose if the \"Hangman\" is completed.");
+        jTextPane1.setPreferredSize(new java.awt.Dimension(300, 200));
         jScrollPane1.setViewportView(jTextPane1);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 301, Short.MAX_VALUE)
-                .addGap(48, 48, 48))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 226, Short.MAX_VALUE)
-                .addGap(41, 41, 41))
-        );
+        getContentPane().add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        jButton2.setText("Exit");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, java.awt.BorderLayout.PAGE_END);
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
